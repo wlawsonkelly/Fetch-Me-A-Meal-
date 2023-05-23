2a5754cb1509460c9479be63bb19a06316b6c6b0
Lawson <wlawsonkelly@gmail.com>
2023-05-23 10:40:35 -0400

    Initial Commit

---
+++ Fetch Me A Meal!/Fetch_Me_A_Meal_App.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_Me_A_Meal_App.swift
+//  Fetch Me A Meal!
+//
+//  Created by Lawson Kelly on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_Me_A_Meal_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fetch Me A Meal!UITests/Fetch_Me_A_Meal_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fetch_Me_A_Meal_UITestsLaunchTests.swift
+//  Fetch Me A Meal!UITests
+//
+//  Created by Lawson Kelly on 5/23/23.
+//
+
+import XCTest
+
+final class Fetch_Me_A_Meal_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
